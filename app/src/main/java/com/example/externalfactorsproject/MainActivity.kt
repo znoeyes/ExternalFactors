876467c5b135430810470359262f15e3cde55501
Seyeon Lee <48233529+znoeyes@users.noreply.github.com>
2020-06-19 00:31:09 +0900

    commit

--- app/src/main/java/com/example/externalfactorsproject/MainActivity.kt
+++ app/src/main/java/com/example/externalfactorsproject/MainActivity.kt
@@ -26,12 +26,10 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val PERMISSION_ID = 1000
-
-    enum class Emotion{
-        SWEET, SORROW, MODERN, ENTHUSIASTIC, WARM
-    }
-    val CITY: String = "seoul,kr"
-    val API: String = "cf47ea0b59553630ae022abdf6c77247"
+    private lateinit var LAT: String
+    private lateinit var LON: String
+    private val CITY: String = "segok-dong,kr"
+    private val API: String = "cf47ea0b59553630ae022abdf6c77247" //OpenWeatherMap 날씨 API
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,17 +46,17 @@ class MainActivity : AppCompatActivity() {
     //마지막으로 알려진 위치 가져오기
     @SuppressLint("MissingPermission")
     private fun getLastLocation(){
-        if (checkPermissions()){
-            if(isLocationEnabled()){ //위치 권한을 수락할 경우 if문 실행
+        if (checkPermissions()){ //위치 권한 검사
+            if(isLocationEnabled()){ //사용자가 '설정'에서 위치 권한을 수락할 경우
                 fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){
                         task -> var location: Location? = task.result
                     if(location == null){
-                        requestNewLocationData()
+                        requestNewLocationData() //실행 중 권한 요청 코드 적용
                     } else { //모든 권한이 승인됐을 때, 출력 코드
                         findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
                         findViewById<TextView>(R.id.lonTextView).text = location.longitude.toString()
-                        val LAT = location.latitude //얘네를 어떻게 반환시켜야하나아아아
-                        val LON = location.longitude
+                        LAT = location.latitude.toString() //intent
+                        LON = location.longitude.toString()
                     }
                 }
             } else {
@@ -67,7 +65,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         } else {
-            requestPermissions()
+            requestPermissions() //이전에 권한을 거부한 적이 있는 경우 권한 요청
         }
     }
 
@@ -142,7 +140,7 @@ class MainActivity : AppCompatActivity() {
         //스레드가 수행할 작업(생성된 스레드)
         override fun doInBackground(vararg params: String?): String? {
             var response:String?
-            try{
+            try{ // lat=$LAT&lon=$LON | q=$CITY
                 response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
             }
             catch (e: Exception){
@@ -160,18 +158,12 @@ class MainActivity : AppCompatActivity() {
                 val main = jsonObj.getJSONObject("main")
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
                 val sys = jsonObj.getJSONObject("sys")
-//                val wind = jsonObj.getJSONObject("wind")
 
                 val updatedAt:Long = jsonObj.getLong("dt")
                 val updatedAtText = "Updated at: "+ SimpleDateFormat("yyyy-MM-dd hh:mm a", Locale.KOREA).format(Date(updatedAt*1000))
                 val weatherDescription = weather.getString("description")
                 val temp = main.getString("temp")+"°C"
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
-//                val pressure = main.getString("pressure")
-//                val humidity = main.getString("humidity")
-//                val sunrise:Long = sys.getLong("sunrise")
-//                val sunset:Long = sys.getLong("sunset")
-//                val windSpeed = wind.getString("speed")
 
 
                 /* Populating extracted data into our views */
@@ -179,11 +171,6 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
                 findViewById<TextView>(R.id.temp).text = temp
                 findViewById<TextView>(R.id.address).text = address
-//                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-//                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-//                findViewById<TextView>(R.id.wind).text = windSpeed
-//                findViewById<TextView>(R.id.pressure).text = pressure
-//                findViewById<TextView>(R.id.humidity).text = humidity
 
                 /* Views populated, Hiding the loader, Showing the main design */
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
