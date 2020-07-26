25c655f72244b29d10e15053903db54330e79be2
Seyeon Lee <seyeon4302@naver.com>
2020-07-26 14:00:35 +0900

    Create activity_07

---
+++ app/src/main/java/com/example/externalfactorsproject/PlayMusicActivity.kt
@@ -0,0 +1,22 @@
+package com.example.externalfactorsproject
+
+//import android.os.Bundle
+//import com.google.android.youtube.player.YouTubeBaseActivity
+//import com.google.android.youtube.player.YouTubePlayer
+//import com.google.android.youtube.player.YouTubePlayerView
+//
+//class PlayMusicActivity : YouTubeBaseActivity() {
+//    private lateinit var playerView: YouTubePlayerView
+//    private lateinit var player: YouTubePlayer
+//    private val API_KEY: String = "AIzaSyB3lgEALNYeCJqKz-SuOBmBfI3mhQ2yLtI"
+//    private var videoId: String = "5a-tqIQc8RM"
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_07_play_music)
+//    }
+//
+//    fun initPlayer(){
+//        playerView = findViewById(R.id.playerView)
+//    }
+//}
